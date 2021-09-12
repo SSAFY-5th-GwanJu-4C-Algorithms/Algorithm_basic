8e0d6c6035c3489173bfcb2668dc049162f4bab1
johyeoncheol <50797070+johyeoncheol@users.noreply.github.com>
2021-09-13 08:53:22 +0900

    백준 16236 N과 M

---
+++ hyeoncheol/Bracktracking/Back_16236.java
@@ -0,0 +1,24 @@
+package com.Back;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Back_16236 {
+	static int N;
+	static int [][]map;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				
+			}
+		}
+	}
+}
